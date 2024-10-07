bad384a0e7185bf06d86e6e30664999626dc104d
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-10-07 08:24:11 +0200

    Applying Code Cleanup.

--- Tests/CK.DB.GuestActor.Acl.Tests/GuestActorAclTests.cs
+++ Tests/CK.DB.GuestActor.Acl.Tests/GuestActorAclTests.cs
@@ -10,49 +10,48 @@ using NUnit.Framework;
 
 using static CK.Testing.MonitorTestHelper;
 
-namespace CK.DB.GuestActor.Tests
+namespace CK.DB.GuestActor.Tests;
+
+[TestFixture]
+public class GuestActorAclTests
 {
-    [TestFixture]
-    public class GuestActorAclTests
+    [Test]
+    public void when_guest_actor_was_granted_on_acl_it_can_be_destroyed()
     {
-        [Test]
-        public void when_guest_actor_was_granted_on_acl_it_can_be_destroyed()
-        {
-            var guestActorTable = SharedEngine.Map.StObjs.Obtain<GuestActorTable>();
-            var aclTable = SharedEngine.Map.StObjs.Obtain<AclTable>();
+        var guestActorTable = SharedEngine.Map.StObjs.Obtain<GuestActorTable>();
+        var aclTable = SharedEngine.Map.StObjs.Obtain<AclTable>();
 
-            using( var ctx = new SqlStandardCallContext( TestHelper.Monitor ) )
-            {
-                var createResult = guestActorTable.CreateGuestActor( ctx, 1, DateTime.Now + TimeSpan.FromMinutes( 5 ), true );
-                var aclId = aclTable.CreateAcl( ctx, 1 );
-                aclTable.AclGrantSet(ctx, 1, aclId, createResult.GuestActorId, "", (byte) GrantLevel.Viewer );
-                guestActorTable
-                   .Invoking( sut => sut.DestroyGuestActor( ctx, 1, createResult.GuestActorId ) )
-                   .Should()
-                   .NotThrow();
-            }
+        using( var ctx = new SqlStandardCallContext( TestHelper.Monitor ) )
+        {
+            var createResult = guestActorTable.CreateGuestActor( ctx, 1, DateTime.Now + TimeSpan.FromMinutes( 5 ), true );
+            var aclId = aclTable.CreateAcl( ctx, 1 );
+            aclTable.AclGrantSet( ctx, 1, aclId, createResult.GuestActorId, "", (byte)GrantLevel.Viewer );
+            guestActorTable
+               .Invoking( sut => sut.DestroyGuestActor( ctx, 1, createResult.GuestActorId ) )
+               .Should()
+               .NotThrow();
         }
+    }
 
-        [Test]
-        public void when_guest_actor_is_a_user_and_was_granted_on_acl_it_can_be_destroyed()
+    [Test]
+    public void when_guest_actor_is_a_user_and_was_granted_on_acl_it_can_be_destroyed()
+    {
+        var userTable = SharedEngine.Map.StObjs.Obtain<UserTable>();
+        var guestActorTable = SharedEngine.Map.StObjs.Obtain<GuestActorTable>();
+        var aclTable = SharedEngine.Map.StObjs.Obtain<AclTable>();
+
+        using( var ctx = new SqlStandardCallContext( TestHelper.Monitor ) )
         {
-            var userTable = SharedEngine.Map.StObjs.Obtain<UserTable>();
-            var guestActorTable = SharedEngine.Map.StObjs.Obtain<GuestActorTable>();
-            var aclTable = SharedEngine.Map.StObjs.Obtain<AclTable>();
-
-            using( var ctx = new SqlStandardCallContext( TestHelper.Monitor ) )
-            {
-                var userId = userTable.CreateUser( ctx, 1, Guid.NewGuid().ToString() );
-                var payload = guestActorTable.CreatePayload();
-                var createResult =  guestActorTable.CreateOrUpdateGuestActor( ctx, 1, userId, payload as IGuestActorInfo, UCLMode.CreateOnly );
-                createResult.OperationResult.Should().Be( UCResult.Created );
-                var aclId = aclTable.CreateAcl( ctx, 1 );
-                aclTable.AclGrantSet(ctx, 1, aclId, userId, "", (byte) GrantLevel.Viewer );
-                userTable
-                   .Invoking( sut => sut.DestroyUser( ctx, 1, userId ) )
-                   .Should()
-                   .NotThrow();
-            }
+            var userId = userTable.CreateUser( ctx, 1, Guid.NewGuid().ToString() );
+            var payload = guestActorTable.CreatePayload();
+            var createResult = guestActorTable.CreateOrUpdateGuestActor( ctx, 1, userId, payload as IGuestActorInfo, UCLMode.CreateOnly );
+            createResult.OperationResult.Should().Be( UCResult.Created );
+            var aclId = aclTable.CreateAcl( ctx, 1 );
+            aclTable.AclGrantSet( ctx, 1, aclId, userId, "", (byte)GrantLevel.Viewer );
+            userTable
+               .Invoking( sut => sut.DestroyUser( ctx, 1, userId ) )
+               .Should()
+               .NotThrow();
         }
     }
 }
